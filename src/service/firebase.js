ac59b7f7c0d15feadce92e3a4f219e10739c2fa7
AnggaTEA <hello.angganugraha@gmail.com>
2021-04-24 13:20:51 +0700

    first commit

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -5,13 +5,13 @@ import "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCe79sGc9CVyvgKGr0CAGpReyB1HmbcAFA",
-  authDomain: "pesansantai-8914a.firebaseapp.com",
-  databaseURL: "https://pesansantai-8914a-default-rtdb.firebaseio.com",
-  projectId: "pesansantai-8914a",
-  storageBucket: "pesansantai-8914a.appspot.com",
-  messagingSenderId: "990487466945",
-  appId: "1:990487466945:web:1a3fc4d605d53eb9c81540",
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
